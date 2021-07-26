2a8110b980b5713b20f64d01d37713972f96f884
Cooper <kcoopermiller9@gmail.com>
2021-07-25 23:26:28 -0500

    initial commit

---
+++ bot.py
@@ -0,0 +1,14 @@
+import os
+from discord.ext import commands
+import os
+from dotenv import load_dotenv
+
+bot = commands.Bot(command_prefix='.')
+
+for filename in os.listdir('cogs'):
+    if filename.endswith('.py'):
+        bot.load_extension(f'cogs.{filename[:-3]}')
+
+load_dotenv()
+TOKEN = os.getenv('TOKEN')
+bot.run(TOKEN)
